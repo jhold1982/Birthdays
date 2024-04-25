2ecdff3868173ebc672928c3b7a531a13aee4cc5
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-04-25 15:46:08 -0500

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Justin Hold on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
